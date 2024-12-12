a5c896a701878faf0aa75c864b9706c33151a6c0
Sávio <115809910+savio27022022@users.noreply.github.com>
2024-12-12 20:34:44 -0300

    Update Create.sql

--- Create.sql
+++ Create.sql
@@ -1,1 +1,40 @@
+-- Criando o banco de dados
+CREATE DATABASE loja_informatica;
 
+-- Usando o banco de dados
+USE loja_informatica;
+
+-- Criando as tabelas
+
+CREATE TABLE clientes (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    cpf VARCHAR(11) NOT NULL UNIQUE,
+    endereco TEXT
+);
+
+CREATE TABLE vendas (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    data_venda DATE NOT NULL,
+    valor_total DECIMAL(10,2) NOT NULL,
+    cliente_id INT,
+    FOREIGN KEY (cliente_id) REFERENCES clientes(id)
+);
+
+CREATE TABLE itens_vendas (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    venda_id INT,
+    produto_id INT,
+    quantidade INT NOT NULL,
+    valor_unitario DECIMAL(10,2) NOT NULL,
+    FOREIGN KEY (venda_id) REFERENCES vendas(id),
+    FOREIGN KEY (produto_id) REFERENCES produtos(id)
+);
+
+CREATE TABLE produtos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    descricao TEXT,
+    preco DECIMAL(10,2) NOT NULL,
+    quantidade_estoque INT NOT NULL
+);
